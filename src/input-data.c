0306171f50fb2eed68365fe9369a37cabefeaa17
Потапов Роман Владимирович <ip812s25@mwp12.csc.local>
2019-04-15 13:00:47 +0700

    created input-data

---
+++ src/input-data.c
@@ -0,0 +1,10 @@
+#include "geometry.h"
+
+int input_data(int f, int f1)
+{
+    if (f1 <= 0 || f <= 0 || f1 > 3 || f > 3) {
+        cout << "ERROR INPUT DATA" << endl;
+        exit(0);
+        return 0;
+    }
+}
