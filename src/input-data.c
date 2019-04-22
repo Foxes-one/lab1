b8b4fcd4714622cf841aa739b5dec7ad23a1c20d
Потапов Роман Владимирович <ip812s25@mwp12.csc.local>
2019-04-22 11:57:08 +0700

    fix input-data

--- src/input-data.c
+++ src/input-data.c
@@ -2,9 +2,8 @@
 
 int input_data(int f, int f1)
 {
-    if (f1 <= 0 || f <= 0 || f1 > 3 || f > 3) {
+    if (f1 < 1 || f < 1 || f1 > 3 || f > 3){
         cout << "ERROR INPUT DATA" << endl;
         exit(0);
-        return 0;
     }
 }
